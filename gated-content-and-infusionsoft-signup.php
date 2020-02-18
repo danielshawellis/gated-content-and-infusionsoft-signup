b41fe4b4a2514207e34672f56e998df187cc2fc3
Daniel Ellis <danielshawellis@gmail.com>
2020-02-18 12:31:20 -0600

    add form template, stylesheet, and script through shortcode

--- gated-content-and-infusionsoft-signup.php
+++ gated-content-and-infusionsoft-signup.php
@@ -14,3 +14,39 @@
 if ( ! defined( 'ABSPATH' ) ) {
   die;
 }
+
+/*
+  Plugin Container Class
+*/
+if ( !class_exists( 'Ammonite_Gated_Content_Infusionsoft_Signup' ) ) {
+  class Ammonite_Gated_Content_Infusionsoft_Signup
+  {
+    // Add shortcode to pull in the form template and its associated scripts and styles
+    public static function add_shortcode_to_include_form_template_and_associated_scripts_and_styles() {
+      add_shortcode( 'gated_content_infusionsoft_signup_form', array( 'Ammonite_Gated_Content_Infusionsoft_Signup', 'include_form_template_and_associated_scripts_and_styles' ) );
+    }
+
+    // Function called whenever the shortcode [gated_content_infusionsoft_signup_form] is used
+    public static function include_form_template_and_associated_scripts_and_styles() {
+      // Include form template wherever shortcode is used
+      include( 'templates/signup-form.php' );
+
+      // Enqueue associated script
+      wp_enqueue_script( 'ammonite_gated_content_infusionsoft_signup_form_script' );
+
+      // Enqueue associated stylesheet
+      wp_enqueue_style( 'ammonite_gated_content_infusionsoft_signup_form_styles' );
+    }
+
+    // Register styles and scripts
+    public static function register_scripts_and_styles() {
+      add_action( 'wp_enqueue_scripts', function() {
+        wp_register_script( 'ammonite_gated_content_infusionsoft_signup_form_script', plugins_url('assets/js/ammonite-gated-content-infusionsoft-signup-form.js', __FILE__ ), array('jquery'), '1.0.0', true );
+        wp_register_style( 'ammonite_gated_content_infusionsoft_signup_form_styles', plugins_url('assets/css/ammonite-gated-content-infusionsoft-signup-form.css', __FILE__ ), array(), '1.0.0', 'all' );
+      } );
+    }
+  }
+
+  Ammonite_Gated_Content_Infusionsoft_Signup::register_scripts_and_styles();
+  Ammonite_Gated_Content_Infusionsoft_Signup::add_shortcode_to_include_form_template_and_associated_scripts_and_styles();
+}
---
+++ templates/signup-form.php
@@ -0,0 +1,35 @@
+<form accept-charset="UTF-8" action="https://ay767.infusionsoft.com/app/form/process/58140e7cf4edfd38020dff5111b0d28b" class="infusion-form" id="inf_form_58140e7cf4edfd38020dff5111b0d28b" method="POST">
+    <input name="inf_form_xid" type="hidden" value="58140e7cf4edfd38020dff5111b0d28b" />
+    <input name="inf_form_name" type="hidden" value="Web Form submitted" />
+    <input name="infusionsoft_version" type="hidden" value="1.70.0.187751" />
+    <div class="infusion-field">
+        <label for="inf_field_FirstName">First Name *</label>
+        <input class="infusion-field-input" id="inf_field_FirstName" name="inf_field_FirstName" placeholder="First Name *" type="text" />
+    </div>
+    <div class="infusion-field">
+        <label for="inf_field_LastName">Last Name *</label>
+        <input class="infusion-field-input" id="inf_field_LastName" name="inf_field_LastName" placeholder="Last Name *" type="text" />
+    </div>
+    <div class="infusion-field">
+        <label for="inf_field_Email">Email *</label>
+        <input class="infusion-field-input" id="inf_field_Email" name="inf_field_Email" placeholder="Email *" type="text" />
+    </div>
+    <div class="">
+      <label>Username *</label>
+      <input placeholder="Username *" type="text" />
+    </div>
+    <div class="">
+      <label>Password *</label>
+      <input placeholder="Password *" type="text" />
+    </div>
+    <div>
+        <div>&nbsp;</div>
+    </div>
+    <div class="infusion-submit">
+        <button class="infusion-recaptcha" id="recaptcha_58140e7cf4edfd38020dff5111b0d28b" type="submit">Submit</button>
+    </div>
+</form>
+<script type="text/javascript" src="https://ay767.infusionsoft.app/app/webTracking/getTrackingCode"></script>
+<script type="text/javascript" src="https://ay767.infusionsoft.com/resources/external/recaptcha/production/recaptcha.js?b=1.70.0.187751-hf-202002041343"></script>
+<script src="https://www.google.com/recaptcha/api.js?onload=onloadInfusionRecaptchaCallback&render=explicit" async="async" defer="defer"></script>
+<script type="text/javascript" src="https://ay767.infusionsoft.com/app/timezone/timezoneInputJs?xid=58140e7cf4edfd38020dff5111b0d28b"></script>
