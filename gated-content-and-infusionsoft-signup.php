9129b870ec6b1690a5c14ea7d9ddd357f1ac5bbc
Daniel Ellis <danielshawellis@gmail.com>
2020-02-18 12:09:45 -0600

    add headers and basic meta information

---
+++ gated-content-and-infusionsoft-signup.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name:       Gated Content/ Infusionsoft Sign Up Form
+ * Description:       This plugin creates a signup form that adds new users in Infusionsoft and creates accounts for them, enabling access to gated content. It requires the plugins Simple WordPress Membership and Simple Membership API to be installed and activated. 
+ * Version:           1.0.0
+ * Author:            Daniel Ellis
+ * Author URI:        https://danielellisdevelopment.com/
+ */
